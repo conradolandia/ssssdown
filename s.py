91091f0184bb130485cc69ca887eaf71a52fa092
Andres Conrado <andresconrado@gmail.com>
2016-10-24 01:36:06 -0500

    Fixed inconsistencies with the download, add folder to download

--- s.py
+++ s.py
@@ -6,7 +6,9 @@ from clint.textui import progress
 import os
 import glob
 
-files = glob.glob('ssss/*.jpg')
+path = 'ssss'
+os.makedirs(path, exist_ok=True)
+files = glob.glob(path + '/*.jpg')
 comicCounter = len(files)  # reads the number of files in the folder to start downloading at the next comic
 print(str(comicCounter) + " files currently in the folder.")
 errors = 0
@@ -16,15 +18,17 @@ def download_comic(url,comicName):
   total_length = int(image.headers.get('content-length'))
   file_type = image.headers.get('content-type')
   status_code = image.status_code
-  if status_code == 200 and file_type == 'image/jpeg':
+
+  if status_code == 200 and total_length > 0 and file_type == 'image/jpeg':
     print('---') 
-    print(str(comicName) + ": Downloading...")
+    print(comicName + ": Downloading...")
     with open(comicName, 'wb') as f:
       for chunk in progress.bar(image.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1): 
         if chunk:
           f.write(chunk)
           f.flush()
-    print(str(comicName) + ": Ready.")
+    print(comicName + ": Ready.")
+    os.rename(comicName, path + '/' + comicName)
 
   else:
     print(str("comic" + ' ' + str(comicCounter) + ' ' + "does not exist"))
@@ -33,9 +37,8 @@ def download_comic(url,comicName):
     errors += 1
 
 while errors == 0:
-  comicNumber = str(comicCounter + 1)
-  comicName = str(comicNumber + ".jpg")  
-  url = str("http://www.sssscomic.com/comicpages/" + comicName)  
   comicCounter += 1
+  comicNumber = comicCounter
+  comicName = str(comicNumber) + ".jpg"
+  url = str("http://www.sssscomic.com/comicpages/" + comicName)  
   download_comic(url,comicName) 
-  
